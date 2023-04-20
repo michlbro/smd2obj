098a2c202958b2340ddfabe17e0d6d65f027dc3b
Michael <96602061+michlbro@users.noreply.github.com>
2023-04-20 05:15:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import SmdLoader
 import ObjCreator
 
-file = r"C:\Users\wnace\Documents\Developing\Assets\Portal\Portal_Decompiled_Models\root\models\props\wall_pipes_terminate01\wall_pipes_terminate01\wall_pipes_terminate01.smd"
+file = r"file.smd"
 export = r""
 
 smd = SmdLoader.smd(file)
 obj = ObjCreator.obj(smd.GetVertexCords(), smd.GetVertexNorm(), smd.GetVertexTex(), smd.GetVertextIndex())
 obj.Convert()
-obj.ExportObj(r'C:\Users\wnace\Documents\Developing\Assets\test', 'test')
+obj.ExportObj(r'output path', 'file name')
