1507ea5f53e8d1d6b1b85b9bbe45e03931e26b7f
KonouChan <wnaces52@gmail.com>
2023-04-21 23:26:05 +0100

    Update smd2obj.py

--- smd2obj.py
+++ smd2obj.py
@@ -2,13 +2,13 @@ import bpy
 import os
 
 smdFiles = r'<file input here>'
-exportFIles = r'<file output here>'
+exportFiles = r'<file output here>'
 for root, _, files in os.walk(smdFiles):
     for name in files:
         if name.endswith('smd'):
             bpy.ops.object.select_all(action='SELECT')
             bpy.ops.object.delete()
             bpy.ops.import_scene.smd(filepath=os.path.join(root, name))
-            os.makedirs(root.replace(smdFiles, exportFIles))
-            bpy.ops.export_scene.obj(filepath=os.path.join(root.replace(smdFiles, exportFIles), name+'.obj'))
-            print(root.replace(smdFiles, exportFIles))
+            os.makedirs(root.replace(smdFiles, exportFiles))
+            bpy.ops.export_scene.obj(filepath=os.path.join(root.replace(smdFiles, exportFiles), name+'.obj'))
+            print(root.replace(smdFiles, exportFiles))
