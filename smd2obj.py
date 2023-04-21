4bd307adc8d57dddb17685a617e61ec9d4a2d27c
KonouChan <wnaces52@gmail.com>
2023-04-21 23:48:21 +0100

    Final Commit I swear

--- smd2obj.py
+++ smd2obj.py
@@ -3,12 +3,16 @@ import os
 
 smdFiles = r'<file input here>'
 exportFiles = r'<file output here>'
+removeMTLFiles = True
+
 for root, _, files in os.walk(smdFiles):
     for name in files:
         if name.endswith('smd'):
+            fileName = root.replace(smdFiles, exportFiles)
             bpy.ops.object.select_all(action='SELECT')
             bpy.ops.object.delete()
             bpy.ops.import_scene.smd(filepath=os.path.join(root, name))
-            os.makedirs(root.replace(smdFiles, exportFiles))
-            bpy.ops.export_scene.obj(filepath=os.path.join(root.replace(smdFiles, exportFiles), name+'.obj'))
-            print(root.replace(smdFiles, exportFiles))
+            os.makedirs(fileName, exist_ok= True)
+            bpy.ops.export_scene.obj(filepath=os.path.join(root.replace(smdFiles, exportFiles), name.replace('.smd', '')+'.obj'))
+            if removeMTLFiles:
+                os.remove(os.path.join(fileName, name.replace('.smd', '')+'.mtl'))
